0a340881a30168384f138a51933d9db6f01a2f92
Subhrakanti Dasgupta <subhrakanti.dasgupta@gmail.com>
2021-10-09 01:18:19 +0530

    Add files via upload

---
+++ BMI Calculator Native App/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import './screens/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI Calculator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.yellow
+      ),
+      home: Home(),
+    );
+  }
+}
