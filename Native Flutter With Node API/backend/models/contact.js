00f67eab416e30a6b0b7e383e46ed7b3fd307c74
Subhrakanti Dasgupta <subhrakanti.dasgupta@gmail.com>
2021-12-04 22:27:51 +0530

    Delete Native Flutter With Node API directory

--- Native Flutter With Node API/backend/models/contact.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const contactSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    mail: { type: String, required: true }
-});
-
-module.exports = mongoose.model("Contact", contactSchema);
--- Native Flutter With Node API/native/android/app/src/main/kotlin/com/example/node_flut/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.node_flut
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
